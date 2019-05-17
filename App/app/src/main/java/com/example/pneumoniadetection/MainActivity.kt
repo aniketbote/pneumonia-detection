9c9b79ebfbea5e04bdbf2b5ffdf7dbdb23696cce
aniketbote2098 <aniketbote2098@gmail.com>
2019-05-18 00:01:17 +0530

    added toast

--- App/app/src/main/java/com/example/pneumoniadetection/MainActivity.kt
+++ App/app/src/main/java/com/example/pneumoniadetection/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.pneumoniadetection
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity","Button waas clicked")
+            Toast.makeText(this, "Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+
+
     }
+
 }
+
